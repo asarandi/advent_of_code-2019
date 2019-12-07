4c0e1e485feb05a17e484e9ea53bc320d7735ee3
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-12-06 16:29:30 -0800

    day 05 cosmetics

--- day_05/part02.go
+++ day_05/part02.go
@@ -2,107 +2,99 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"io/ioutil"
-	"strings"
-	"strconv"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "strconv"
+    "strings"
 )
 
-/*
+func get_params(array []int, index int) (int, int, int, int) {
+    var size, i, j, k int
+    instructionLengths := map[int]int{
+        1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 99: 1}
 
-opcode 1 - add parameter 1 to parameter 2, store result at parameter 3
-opcode 2 - multiply parameter 1 to parameter 2, store result at parameter 3
-opcode 3 - save input to parameter
-opcode 4 - output parameter
-opcode 5 - jump if true
-opcode 6 - jump if false
-opcode 7 - less than
-opcode 8 - equals
-opcode 99 - halt
-
-*/
-
-func p1(x int) int {
-	return (x / 100) % 10
+    size = instructionLengths[array[index]%100]
+    if size >= 2 {
+        if array[index]/100%10 == 1 {
+            i = index + 1
+        } else {
+            i = array[index+1]
+        }
+    }
+    if size >= 3 {
+        if array[index]/1000%10 == 1 {
+            j = index + 2
+        } else {
+            j = array[index+2]
+        }
+    }
+    if size >= 4 {
+        k = array[index+3]
+    }
+    return size, i, j, k
 }
 
-func p2(x int) int {
-	return (x / 1000) % 10
+func exec(array []int, input int) {
+    done := false
+    for index := 0; !done; {
+        size, i, j, k := get_params(array, index)
+        switch (array[index] % 100) {
+        case 1:
+            array[k] = array[i] + array[j]
+        case 2:
+            array[k] = array[i] * array[j]
+        case 3:
+            array[i] = input
+        case 4:
+            fmt.Println("output", array[i])
+        case 5:
+            if array[i] != 0 {
+                index = array[j] - size
+            }
+        case 6:
+            if array[i] == 0 {
+                index = array[j] - size
+            }
+        case 7:
+            if array[i] < array[j] {
+                array[k] = 1
+            } else {
+                array[k] = 0
+            }
+        case 8:
+            if array[i] == array[j] {
+                array[k] = 1
+            } else {
+                array[k] = 0
+            }
+        case 99:
+            done = true
+        default:
+            log.Fatal("error")
+        }
+        index += size
+    }
 }
-
-func p3(x int) int {
-	return (x / 10000) % 10
-}
-
 
 func main() {
-	content, err := ioutil.ReadFile("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	s := strings.Trim(string(content), " \t\n\r\v\f");
-	split := strings.Split(s, ",")
-	a := make([]int, len(split))
-	for idx, v := range split {
-		i, err := strconv.Atoi(v)
-		if err != nil {
-			log.Fatal(err)
-		}
-		a[idx] = i
-	}
-	done := false
-	for i:=0; !done; {
-		var val1, val2, size int
-		switch(a[i] % 100) {
-		case 1:
-			size = 4
-			if p1(a[i])==1 { val1 = a[i+1] } else { val1 = a[a[i+1]] }
-			if p2(a[i])==1 { val2 = a[i+2] } else { val2 = a[a[i+2]] }
-			a[a[i+3]] = val1 + val2
-		case 2:
-			size = 4
-			if p1(a[i])==1 { val1 = a[i+1] } else { val1 = a[a[i+1]] }
-			if p2(a[i])==1 { val2 = a[i+2] } else { val2 = a[a[i+2]] }
-			a[a[i+3]] = val1 * val2
-		case 3:
-			size = 2
-			a[a[i+1]] = 5	/* XXX */
-		case 4:
-			size = 2
-			if p1(a[i])==1 { val1 = a[i+1] } else { val1 = a[a[i+1]] }
-			fmt.Println("output", val1)
+    content, err := ioutil.ReadFile("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    s := strings.Trim(string(content), " \t\n\r\v\f");
+    split := strings.Split(s, ",")
+    array := make([]int, len(split))
+    for idx, v := range split {
+        i, err := strconv.Atoi(v)
+        if err != nil {
+            log.Fatal(err)
+        }
+        array[idx] = i
+    }
+    array_copy := make([]int, len(array))
+    copy(array_copy, array)
 
-		case 5:
-			size = 3
-			if p1(a[i])==1 { val1 = a[i+1] } else { val1 = a[a[i+1]] }
-			if p2(a[i])==1 { val2 = a[i+2] } else { val2 = a[a[i+2]] }
-			if val1 != 0 { i = val2 - size }
-
-		case 6:
-			size = 3
-			if p1(a[i])==1 { val1 = a[i+1] } else { val1 = a[a[i+1]] }
-			if p2(a[i])==1 { val2 = a[i+2] } else { val2 = a[a[i+2]] }
-			if val1 == 0 { i = val2 - size }
-
-		case 7:
-			size = 4
-			if p1(a[i])==1 { val1 = a[i+1] } else { val1 = a[a[i+1]] }
-			if p2(a[i])==1 { val2 = a[i+2] } else { val2 = a[a[i+2]] }
-			if val1 < val2 { a[a[i+3]] = 1 } else { a[a[i+3]] = 0 }
-
-		case 8:
-			size = 4
-			if p1(a[i])==1 { val1 = a[i+1] } else { val1 = a[a[i+1]] }
-			if p2(a[i])==1 { val2 = a[i+2] } else { val2 = a[a[i+2]] }
-			if val1 == val2 { a[a[i+3]] = 1 } else { a[a[i+3]] = 0 }
-
-		case 99:
-			size = 1
-			done = true
-		default:
-			log.Fatal("error")
-		}
-		i += size
-	}
+    exec(array, 1)
+    exec(array_copy, 5)
 }

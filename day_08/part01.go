d8f45feaea31a555ed7cf7a61f1a866fc4cd8d9e
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-12-07 22:14:56 -0800

    day 08, simplify

--- day_08/part01.go
+++ day_08/part01.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"io/ioutil"
 	"strings"
-	_"strconv"
 	"math"
 )
 
@@ -16,34 +15,27 @@ func main() {
 		log.Fatal(err)
 	}
 	s := strings.Trim(string(content), " \t\n\r\v\f")
-	data := make([]int, len(s))
-	for i:=0; i<len(s); i++ {
-		data[i] = int(s[i] - '0')
-	}
 
-	var i, j, zeros, ones, twos int
-
-	mi := math.MaxInt32
+	minZeros := math.MaxInt32
 	res := math.MaxInt32
 
-	for j=0; j<len(data); {
-		zeros, ones, twos = 0, 0, 0
-		for i=0; i<25*6; i++ {
-			if data[i+j] == 0 {
+	for i:=0; i<len(s);i+=150 {
+		zeros, ones, twos := 0, 0, 0
+		for j:=0; j<150; j++ {
+			if s[i+j] == '0' {
 				zeros++
 			}
-			if data[i+j] == 1 {
+			if s[i+j] == '1' {
 				ones++
 			}
-			if data[i+j] == 2 {
+			if s[i+j] == '2' {
 				twos++
 			}
 		}
-		if zeros < mi {
-			mi = zeros
+		if zeros < minZeros {
+			minZeros = zeros
 			res = ones * twos
 		}
-		j+=25*6
 	}
 	fmt.Println(res)
 }

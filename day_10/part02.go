65b406d2497d60dcf84069e3e55a28ca01df0167
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-12-10 20:35:41 -0800

    day 10 cosmetics

--- day_10/part02.go
+++ day_10/part02.go
@@ -2,113 +2,117 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"sort"
-	"strings"
-	"math"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "math"
+    "sort"
+    "strings"
 )
 
 type asteroid struct {
-	x, y int
-	d int
-	a float64
+    x, y, n, d int
+    a          float64
 }
 
 var asteroids []asteroid
 
 func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
+    if a < 0 {
+        return -a
+    }
+    return a
 }
 
-func (a asteroid) distance (b asteroid) int {
-	return abs(a.x-b.x) + abs(a.y-b.y)
+func (a asteroid) distance(b asteroid) int {
+    return abs(a.x-b.x) + abs(a.y-b.y)
 }
 
-func (a asteroid) angle (b asteroid) float64 {
-	return math.Atan2(float64(a.x-b.x), float64(a.y-b.y))
+func (a asteroid) angle(b asteroid) float64 {
+    return math.Atan2(float64(a.x-b.x), float64(a.y-b.y))
 }
 
-type ByDistance []asteroid
-type ByAngle    []asteroid
-
-func (a ByDistance) Len() int           { return len(a) }
-func (a ByDistance) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByDistance) Less(i, j int) bool {
-	if a[i].d == a[j].d {
-		return a[i].a < a[j].a
-	}
-	return a[i].d < a[j].d
+func (a asteroid) is(b asteroid) bool {
+    return a.x == b.x && a.y == b.y
 }
 
-func (a ByAngle)    Len() int           { return len(a) }
-func (a ByAngle)    Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByAngle)    Less(i, j int) bool {
-	if a[i].a == a[j].a {
-		return a[i].d < a[j].d
-	}
-	return a[i].a < a[j].a
+func (a asteroid) inQ1() bool {
+    if a.a >= math.Pi/2 && a.a <= math.Pi {
+        return true
+    }
+    return false
 }
 
-var base = asteroid{22,17,0,0}
+type ByAnglesDistance []asteroid
+func (a ByAnglesDistance) Len() int      { return len(a) }
+func (a ByAnglesDistance) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByAnglesDistance) Less(i, j int) bool {
+    if a[i].a == a[j].a {
+        return a[i].d < a[j].d
+    }
+    return a[i].a < a[j].a
+}
+
+var mapIntAsteroid map[int]asteroid
+var asteroid_id = 1
+
+func setAsteroidIds(base asteroid, cond bool) bool {
+    res := false
+    angles := make(map[float64]bool)
+    for i := range asteroids {
+        if asteroids[i].inQ1() != cond ||
+            asteroids[i].n != 0 ||
+            base.is(asteroids[i]) ||
+            angles[asteroids[i].a] {
+            continue
+        }
+        angles[asteroids[i].a] = true
+        mapIntAsteroid[asteroid_id] = asteroids[i]
+        asteroids[i].n = asteroid_id
+        asteroid_id += 1
+        res = true
+    }
+    return res
+}
 
 func main() {
-	content, err := ioutil.ReadFile("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	s := strings.Trim(string(content), " \t\r\n\v\f")
-	split := strings.Split(s, "\n")
-	asteroids = make([]asteroid,0)
-	for x, _ := range split {
-		for y, val := range split[x] {
-            if x == base.x && y == base.y {
-                continue
+    content, err := ioutil.ReadFile("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    s := strings.Trim(string(content), " \t\r\n\v\f")
+    split := strings.Split(s, "\n")
+    asteroids = make([]asteroid, 0)
+    for x, _ := range split {
+        for y, val := range split[x] {
+            if val == '#' {
+                asteroids = append(asteroids, asteroid{x, y, 0, 0, 0})
             }
-			if val == '#' {
-				ast := asteroid{x,y,0,0}
-				ast.d = base.distance(ast)
-				ast.a = base.angle(ast)
-				asteroids = append(asteroids,ast)
-			}
-		}
-	}
-
-    count:=0
-	sort.Sort(ByDistance(asteroids))
-	sort.Sort(ByAngle(asteroids))
-
-    angles := make(map[float64]bool)
-
-	for i:=0; i<len(asteroids); i++ {
-		if asteroids[i].a < math.Pi/2 || angles[asteroids[i].a] {
-			continue
-		}
-        angles[asteroids[i].a] = true
-        asteroids = append(asteroids[:i], asteroids[i+1:]...)
-        i--
-        count += 1
-    }
-//    fmt.Println(count)
-
-	for i:=0; i<len(asteroids); i++ {
-		if asteroids[i].a >= math.Pi/2 || angles[asteroids[i].a] {
-			continue
-		}
-        angles[asteroids[i].a] = true
-        if count + 1 == 200 {
-            fmt.Println("result", asteroids[i].y*100+asteroids[i].x)
-            break
         }
-        asteroids = append(asteroids[:i], asteroids[i+1:]...)
-        i--
-        count += 1
     }
-
-//    fmt.Println(count)
-
+    base := asteroids[0]
+    base_num_angles := 0
+    for _, a := range asteroids {
+        angles := make(map[float64]bool)
+        for _, b := range asteroids {
+            angles[a.angle(b)] = true
+        }
+        if len(angles) > base_num_angles {
+            base = a
+            base_num_angles = len(angles)
+        }
+    }
+    fmt.Println("part 1:", base_num_angles)
+    for i := range asteroids {
+        asteroids[i].d = base.distance(asteroids[i])
+        asteroids[i].a = base.angle(asteroids[i])
+    }
+    sort.Sort(ByAnglesDistance(asteroids))
+    mapIntAsteroid = make(map[int]asteroid)
+    for cont := true; cont; {
+        cont = cont && setAsteroidIds(base, true)
+        cont = cont && setAsteroidIds(base, false)
+    }
+    a := mapIntAsteroid[200]
+    fmt.Println("part 2:", a.y*100+a.x)
 }

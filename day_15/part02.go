05cf2953d6dedd3eef7e7f3441a2e8c8caf00478
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-12-15 04:02:09 -0800

    day 15

--- day_15/part02.go
+++ day_15/part02.go
@@ -2,16 +2,12 @@
 package main
 
 import (
+    "container/list"
     "fmt"
     "io/ioutil"
     "log"
     "strconv"
     "strings"
-	_"os"
-	_"bufio"
-	_"time"
-	_"math/rand"
-	"container/list"
 )
 
 func getParams(array []int64, index, base int64) (int64, int64, int64, int64) {
@@ -50,12 +46,11 @@ func getParams(array []int64, index, base int64) (int64, int64, int64, int64) {
     return size, i, j, k
 }
 
-var finished = false
-var want = false
+var isFinished = false
 
-func exec(array []int64, in, out chan int64) {
-    var index, size, i, j, k, base int64
-    for index = 0; !finished; {
+func exec(array []int64, index int64) (bool, int64, int64) {
+    var size, i, j, k, base int64
+    for ; !isFinished; {
         size, i, j, k = getParams(array, index, base)
         switch array[index] % 100 {
         case 1:
@@ -63,10 +58,9 @@ func exec(array []int64, in, out chan int64) {
         case 2:
             array[k] = array[i] * array[j]
         case 3:
-			want = true
-            array[i] = <-in
+            return true, i, index + size
         case 4:
-            out <- array[i]
+            return false, i, index + size
         case 5:
             if array[i] != 0 {
                 index = array[j] - size
@@ -90,46 +84,41 @@ func exec(array []int64, in, out chan int64) {
         case 9:
             base += array[i]
         case 99:
-            finished = true
+            isFinished = true
         default:
             log.Fatal("error")
         }
         index += size
     }
+    return false, -1, -1
 }
 
 type point struct {
     y, x int
 }
 
-func (p1 point) add (p2 point) point {
-	return point{p1.y + p2.y, p1.x + p2.x}
+func (p1 point) add(p2 point) point {
+    return point{p1.y + p2.y, p1.x + p2.x}
 }
 
-var visited map[point]byte
-var current_y, current_x int
-var directions = [4]point{{-1,0}, {1,0}, {0,-1}, {0,1}}
+var area map[point]int64
+var directions = [4]point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 var stack *list.List
-var backtrack bool
 
-func next_step() int {
-	pt := point{current_y, current_x}
-	for k:=0;k<4;k++ {
-		p := pt.add(directions[k])
-		_, ok := visited[p]
-		if ok {
-			continue
-		}
-		backtrack = false
-		return k
-	}
-	if stack.Len() == 0 {
-		finished = true
-		return 0
-	}
-	k := stack.Remove(stack.Front())
-	backtrack = true
-	return k.(int) ^ 1
+func nextStep(currentPos point) (bool, int64) {
+    for k := 0; k < 4; k++ {
+        candidatePos := currentPos.add(directions[k])
+        if _, ok := area[candidatePos]; ok {
+            continue
+        }
+        return false, int64(k)
+    }
+    if stack.Len() == 0 {
+        isFinished = true
+        return false, 0
+    }
+    k := stack.Remove(stack.Front())
+    return true, k.(int64) ^ 1
 }
 
 func main() {
@@ -137,7 +126,7 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-    s := strings.Trim(string(content), " \t\n\r\v\f");
+    s := strings.Trim(string(content), " \t\n\r\v\f")
     split := strings.Split(s, ",")
     array := make([]int64, len(split)*16)
     for idx, v := range split {
@@ -148,80 +137,55 @@ func main() {
         array[idx] = i
     }
 
-    i, j := 0,0
-	current_y, current_x = i,j
-
-    finished = false
-	visited = make(map[point]byte)
-	stack = list.New()
-    in := make(chan int64)
-    out := make(chan int64)
-	dir := -1
-	res := -1
-	var oxy point
-    go exec(array, in, out)
-    for ; !finished; {
-		if want {
-			want = false
-			dir = next_step()
-			i, j = current_y, current_x
-			i += directions[dir].y
-			j += directions[dir].x
-			in <- int64(dir + 1)
-		}
-
-        select {
-        case o := <-out:
-			switch o {
-				case 0: o = '#'
-				case 1: o = '.'
-					current_y, current_x = i, j
-					if !backtrack {
-						stack.PushFront(dir)
-					}
-				case 2: o = 'X'
-					current_y, current_x = i, j
-					if !backtrack {
-						stack.PushFront(dir)
-					}
-					oxy = point{current_y, current_x}
-					res = stack.Len()
-			default:
-				fmt.Println("error")
-				break
-			}
-			visited[point{i,j}] = byte(o)
-        default:
-            break
+    currentPos, oxygenPos := point{0, 0}, point{0, 0}
+    candidatePos := currentPos.add(directions[0])
+    wantInput, backtracking := false, false
+    area = make(map[point]int64)
+    stack = list.New()
+    var index, counter, dir, numSteps int64
+    for ; !isFinished; {
+        wantInput, index, counter = exec(array, counter)
+        if wantInput {
+            backtracking, dir = nextStep(currentPos)
+            array[index] = dir + 1
+            candidatePos = currentPos.add(directions[dir])
+        } else {
+            if array[index] == 1 || array[index] == 2 {
+                currentPos = candidatePos
+                if !backtracking {
+                    stack.PushFront(dir)
+                }
+                if array[index] == 2 {
+                    oxygenPos = candidatePos
+                    numSteps = int64(stack.Len())
+                }
+            }
+            area[candidatePos] = array[index]
         }
-
     }
-    close(in)
-    close(out)
-	fmt.Println("part 1:", res)
+    fmt.Println("part 1:", numSteps)
 
-
-	oxygen := make(map[point]int)
-	queue := list.New()
-	oxygen[oxy] = 0
-	queue.PushFront(oxy)
-	max_g := 0
-	for ; queue.Len() > 0 ; {
-		parent := queue.Remove(queue.Front()).(point)
-		if oxygen[parent] > max_g {
-			max_g = oxygen[parent]
-		}
-		for k:=0; k<4; k++ {
-			child := parent.add(directions[k])
-			if visited[child] != '.' {
-				continue
-			}
-			if _, ok := oxygen[child]; ok {
-				continue
-			}
-			oxygen[child] = oxygen[parent] + 1
-			queue.PushFront(child)
-		}
-	}
-	fmt.Println("part 2:", max_g)
+    oxygen := make(map[point]int)
+    queue := list.New()
+    oxygen[oxygenPos] = 0
+    queue.PushFront(oxygenPos)
+    maxG := 0
+    for ; queue.Len() > 0; {
+        parent := queue.Remove(queue.Front()).(point)
+        if oxygen[parent] > maxG {
+            maxG = oxygen[parent]
+        }
+        for k := 0; k < 4; k++ {
+            child := parent.add(directions[k])
+            if area[child] != 1 {
+                continue
+            }
+            if _, ok := oxygen[child]; ok {
+                continue
+            }
+            oxygen[child] = oxygen[parent] + 1
+            queue.PushFront(child)
+        }
+    }
+    fmt.Println("part 2:", maxG)
 }

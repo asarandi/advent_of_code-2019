aa3d9e279978319bf9c5f366802bea6ad02f4aa0
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-12-30 18:45:37 -0800

    day 24, part 2

--- day_24/part02.go
+++ day_24/part02.go
@@ -1,11 +1,11 @@
-/* advent of code 2019: day 24, part 1 */
+/* advent of code 2019: day 24, part 2 */
 package main
 
 import (
+    "container/list"
     "fmt"
     "io/ioutil"
     "strings"
-    "container/list"
 )
 
 const SIZE = 5
@@ -22,6 +22,19 @@ func (b board) toString() string {
     return res
 }
 
+func fromString(s string) board {
+    trim := strings.Trim(s, " \t\r\n\v\f")
+    split := strings.Split(trim, "\n")
+    data := make([][]byte, len(split))
+    for i := range split {
+        data[i] = make([]byte, len(split[i]))
+        for j := range split[i] {
+            data[i][j] = split[i][j]
+        }
+    }
+    return data
+}
+
 func emptyBoard() board {
     res := board{}
     for i := 0; i < SIZE; i++ {
@@ -33,11 +46,10 @@ func emptyBoard() board {
     return res
 }
 
-func (bo *board) countBugs() int {
-    b := *bo
+func (b board) countBugs() int {
     res := 0
-    for i:=0; i<len(b); i++ {
-        for j:=0; j<len(b[i]); j++ {
+    for i := 0; i < len(b); i++ {
+        for j := 0; j < len(b[i]); j++ {
             if b[i][j] == BUG {
                 res++
             }
@@ -46,23 +58,66 @@ func (bo *board) countBugs() int {
     return res
 }
 
-func (bo *board) countAdjacent(parent, child board, i, j int) int {
-    b := *bo
-    if parent == nil { parent = emptyBoard() }
-    if child == nil { child = emptyBoard() }
+func (b board) countAdjacent(parent, child board, i, j int) int {
+    if parent == nil {
+        parent = emptyBoard()
+    }
+    if child == nil {
+        child = emptyBoard()
+    }
     res := 0
-    if i == 0 && parent[1][2] == BUG { res++ }
-    if j == 0 && parent[2][1] == BUG { res++ }
-    if i == 4 && parent[3][2] == BUG { res++ }
-    if j == 4 && parent[2][3] == BUG { res++ }
-    if i == 1 && j == 2 { for k:=0; k<SIZE; k++ { if child[0][k] == BUG { res++ } } }
-    if i == 2 && j == 1 { for k:=0; k<SIZE; k++ { if child[k][0] == BUG { res++ } } }
-    if i == 2 && j == 3 { for k:=0; k<SIZE; k++ { if child[k][4] == BUG { res++ } } }
-    if i == 3 && j == 2 { for k:=0; k<SIZE; k++ { if child[4][k] == BUG { res++ } } }
-    if i > 0 && b[i-1][j] == BUG { res++ }
-    if j > 0 && b[i][j-1] == BUG { res++ }
-    if i+1 < SIZE && b[i+1][j] == BUG { res++ }
-    if j+1 < SIZE && b[i][j+1] == BUG { res++ }
+    if i == 0 && parent[1][2] == BUG {
+        res++
+    }
+    if j == 0 && parent[2][1] == BUG {
+        res++
+    }
+    if i == 4 && parent[3][2] == BUG {
+        res++
+    }
+    if j == 4 && parent[2][3] == BUG {
+        res++
+    }
+    if i == 1 && j == 2 {
+        for k := 0; k < SIZE; k++ {
+            if child[0][k] == BUG {
+                res++
+            }
+        }
+    }
+    if i == 2 && j == 1 {
+        for k := 0; k < SIZE; k++ {
+            if child[k][0] == BUG {
+                res++
+            }
+        }
+    }
+    if i == 2 && j == 3 {
+        for k := 0; k < SIZE; k++ {
+            if child[k][4] == BUG {
+                res++
+            }
+        }
+    }
+    if i == 3 && j == 2 {
+        for k := 0; k < SIZE; k++ {
+            if child[4][k] == BUG {
+                res++
+            }
+        }
+    }
+    if i > 0 && b[i-1][j] == BUG {
+        res++
+    }
+    if j > 0 && b[i][j-1] == BUG {
+        res++
+    }
+    if i+1 < SIZE && b[i+1][j] == BUG {
+        res++
+    }
+    if j+1 < SIZE && b[i][j+1] == BUG {
+        res++
+    }
     return res
 }
 
@@ -86,51 +141,38 @@ func (b board) age(parent, child board) board {
     return res
 }
 
-func fromString(s string) board {
-    trim := strings.Trim(s, " \t\r\n\v\f")
-    split := strings.Split(trim, "\n")
-    data := make([][]byte, len(split))
-    for i := range split {
-        data[i] = make([]byte, len(split[i]))
-        for j := range split[i] {
-            data[i][j] = split[i][j]
-        }
-    }
-    return data
-}
-
 func main() {
-    content, err := ioutil.ReadFile("sample_01.txt")
+    content, err := ioutil.ReadFile("input.txt")
     if err != nil {
         panic(err)
     }
     b := fromString(string(content))
     lst := list.New()
     lst.PushBack(b)
-
-    for i:=0; i<10; i++ {
-        res, j := 0, 0
-        rep := list.New()
-        rep.PushBack(emptyBoard())
+    for i := 0; i < 200; i++ {
+        var newLst = list.New()
         for e := lst.Front(); e != nil; e = e.Next() {
             var node, parent, child board
-            node, parent, child = nil, nil, nil
-            node = e.Value.(board)
+            node, parent, child = e.Value.(board), nil, nil
             if e.Prev() != nil {
                 parent = e.Prev().Value.(board)
             }
             if e.Next() != nil {
                 child = e.Next().Value.(board)
             }
-            k := node.age(parent, child)
-            rep.PushBack(k)
-            res += k.countBugs()
-            fmt.Println("iteration", i, "item", j); j++
-            fmt.Printf(k.toString())
-            fmt.Println()
-//            fmt.Println("lst.Len()", lst.Len(), "iteration", i, "res", res)
+            if parent == nil {
+                newLst.PushBack(emptyBoard().age(nil, node))
+            }
+            newLst.PushBack(node.age(parent, child))
+            if child == nil {
+                newLst.PushBack(emptyBoard().age(node, nil))
+            }
         }
-        rep.PushBack(emptyBoard())
-        lst = rep
+        lst = newLst
     }
+    res := 0
+    for e := lst.Front(); e != nil; e = e.Next() {
+        res += e.Value.(board).countBugs()
+    }
+    fmt.Println("part 2:", res)
 }
